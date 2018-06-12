3c88ec65f3bac202af5ee0f8e5d92d6fa035baf0
Huzaifa Aejaz <huzaifa.aejaz@sjsu.edu>
2018-06-12 15:50:56 -0700

    Tree

---
+++ src/Fundementals/Tree.java
@@ -0,0 +1,24 @@
+package Fundementals;
+
+/**
+ * Created by huzaifa.aejaz on 6/11/18.
+ */
+public class Tree {
+   class Node{
+       private int val;
+       Node left;
+       Node right;
+       public Node(int val){
+           this.val = val;
+           this.left = null;
+           this.right = null;
+       }
+
+   }
+
+   public Tree(int val){
+       Node node;
+   }
+
+
+}
